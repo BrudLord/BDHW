38e214d0bdba5e25c15f3ead8a186fcc38bac647
Eugene Akimov <akimoveugeny.04@mail.ru>
2024-12-11 23:40:38 +0300

    fix query 6 -- 3

--- 6-3.sql
+++ 6-3.sql
@@ -6,13 +6,9 @@ SET search_path = music_service;
 SELECT
     u.id AS user_id,
     u.email AS user_email,
-    AVG(SUM(sc.cost)) OVER (PARTITION BY u.id) AS avg_purchase_cost
+    AVG(sc.cost) OVER (PARTITION BY u.id) AS avg_purchase_cost
 FROM
     "user" u
     JOIN purchase p ON u.id = p.user_id
     JOIN purchase_song ps ON p.id = ps.purchase_id
-    JOIN song_cost sc ON ps.song_cost_id = sc.id
-GROUP BY
-    u.id,
-    u.email,
-    p.id;
+    JOIN song_cost sc ON ps.song_cost_id = sc.id;
