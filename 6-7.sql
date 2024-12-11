b2aeff708c3dd6a4a04ef7a452033c475b3a3dd3
Eugene Akimov <akimoveugeny.04@mail.ru>
2024-12-11 23:19:28 +0300

    add more complex queries to task 6

---
+++ 6-7.sql
@@ -0,0 +1,22 @@
+-- Запрос 7: Актуальная цена песен с учётом версионности
+-- Запрос выводит список песен с их актуальной ценой. Цена берется из последней версии стоимости, что достигается через использование версионности.
+
+SET search_path = music_service;
+
+SELECT
+    s.id AS song_id,
+    s.title AS song_title,
+    sc.cost AS current_cost,
+    sc.version AS current_version
+FROM
+    song s
+    JOIN song_cost sc ON s.actual_song_cost_id = sc.id
+WHERE
+    sc.version = (
+        SELECT
+            MAX(version)
+        FROM
+            song_cost sc2
+        WHERE
+            sc2.id = sc.id
+    );
